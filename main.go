6ca85ba9684086ffd154f918566a905b7112bc63
chashkakefira <danieldzen@yandex.ru>
2025-08-25 11:57:20 +0300

    Changed CORS Allow-Origin to * (for development)

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 }
 
 func generateSeatingHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	if r.Method == http.MethodOptions {
