416ba7b59ffd4d3799296e8e7acfa401e73eb978
chashkakefira <danieldzen@yandex.ru>
2025-09-09 14:38:33 +0300

    .env configuration file

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	//"net/http"
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"seating-generator/ga"
+
+	"github.com/joho/godotenv"
 )
 
 type Response struct {
@@ -15,15 +17,19 @@ type Response struct {
 }
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Could not load .env file, using defaults")
+	}
 	http.HandleFunc("/generate-seating", generateSeatingHandler)
-	log.Println("Starting server on localhost:5000")
-	if err := http.ListenAndServe(":5000", nil); err != nil {
+	log.Println("Starting server on " + os.Getenv("ALLOWED_ORIGIN"))
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func generateSeatingHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Origin", os.Getenv("ALLOWED_ORIGIN"))
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	if r.Method == http.MethodOptions {
