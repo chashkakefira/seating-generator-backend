a5a482ceed8ad1aebd130e903baadf0540fe770c
chashkakefira <danieldzen@yandex.ru>
2025-12-21 12:23:46 +0300

    feat: dynamic penalty
    - new structures to handle dynamic penalties: PriorityWeights and Weights
    - now GA settings are in PriorityWeights and represented by float numbers from 0 to 10
    - new function calculateWeights, which calculates weights according to user's preferences
    
    Improves algorithm's solution overall

--- ga/ga.go
+++ ga/ga.go
@@ -26,7 +26,7 @@ type Request struct {
 	PopSize         int
 	Generations     int
 	CrossOverChance float64
-	Priority        []int
+	PriorityWeights PriorityWeights
 }
 
 type Response struct {
@@ -35,6 +35,48 @@ type Response struct {
 	Column    int
 	Student   string
 	StudentID int
+}
+
+type PriorityWeights struct {
+	Medical     float64
+	Preferences float64
+	Friends     float64
+	Enemies     float64
+	Fill        float64
+}
+
+type Weights struct {
+	RowBonus     int64
+	PosBonus     int64
+	MedPenalty   int64
+	FriendBonus  int64
+	EnemyPenalty int64
+	PrefBonus    int64
+}
+
+func calculateWeights(config ClassConfig, pw PriorityWeights) Weights {
+	const (
+		BASE_FILL   = 1000
+		BASE_PREF   = 2000
+		BASE_FRIEND = 5000
+		BASE_HARD   = 10000
+	)
+
+	wFill := int64(pw.Fill * float64(BASE_FILL))
+	wPref := int64(pw.Preferences * float64(BASE_PREF))
+	wFriend := int64(pw.Friends * float64(BASE_FRIEND))
+	wMed := int64(pw.Medical * float64(BASE_HARD))
+	wEnemy := int64(pw.Enemies * float64(BASE_HARD))
+
+	return Weights{
+		RowBonus: int64(config.Columns) * wFill * 10,
+		PosBonus: wFill,
+
+		PrefBonus:    wPref,
+		EnemyPenalty: wEnemy,
+		FriendBonus:  wFriend,
+		MedPenalty:   wMed,
+	}
 }
 
 func contains(s []int, elem int) bool {
@@ -46,7 +88,7 @@ func contains(s []int, elem int) bool {
 	return false
 }
 
-func fitness(seating []int, students []Student, preferences, forbidden [][]int, config ClassConfig, priority []int) (int64, []int) {
+func fitness(seating []int, students []Student, preferences, forbidden [][]int, config ClassConfig, w Weights) (int64, []int) {
 	ignored := make([]int, 0)
 	score := 0
 	for i, studentIndex := range seating {
@@ -56,24 +98,21 @@ func fitness(seating []int, students []Student, preferences, forbidden [][]int, 
 		student := students[studentIndex]
 		row := i / config.Columns
 		col := i % config.Columns
-		if (len(student.PreferredRows) > 0 && !contains(student.PreferredRows, row)) || len(student.PreferredColumns) > 0 && !contains(student.PreferredColumns, col) {
-			score -= priority[1] * config.Rows
+		score += (config.Rows - row) * int(w.RowBonus)
+		score += (config.Columns - col) * int(w.PosBonus)
+		if (len(student.PreferredRows) > 0 && contains(student.PreferredRows, row)) || len(student.PreferredColumns) > 0 && contains(student.PreferredColumns, col) {
+			score += int(w.PrefBonus) * config.Rows
 			ignored = append(ignored, student.ID)
-		} else if len(student.PreferredRows) > 0 || len(student.PreferredColumns) > 0 {
-			score += priority[1] * config.Rows
 		}
 
 		if len(student.MedicalPreferredColumns) > 0 && !contains(student.MedicalPreferredColumns, col) || len(student.MedicalPreferredRows) > 0 && !contains(student.MedicalPreferredRows, row) {
-			score -= priority[0]
+			score -= int(w.MedPenalty)
 		}
 
 	}
 	for row := 0; row < config.Rows; row++ {
 		for col := 0; col+1 < config.Columns; col++ {
 			i := row*config.Columns + col
-			if seating[i] < len(students) {
-				score += (config.Rows*config.Columns - i)
-			}
 			if i+1 >= len(seating) || col%2 != 0 || col+1 >= config.Columns {
 				continue
 			}
@@ -86,14 +125,14 @@ func fitness(seating []int, students []Student, preferences, forbidden [][]int, 
 			i2ID := students[seating[i+1]].ID
 			for _, pref := range preferences {
 				if (pref[0] == i1ID && pref[1] == i2ID) || (pref[0] == i2ID && pref[1] == i1ID) {
-					score += config.Rows * priority[3]
+					score += int(w.FriendBonus)
 				} else if pref[0] == i1ID || pref[1] == i1ID || pref[0] == i2ID || pref[1] == i2ID {
 					ignored = append(ignored, i1ID, i2ID)
 				}
 			}
 			for _, forb := range forbidden {
 				if (forb[0] == i1ID && forb[1] == i2ID) || (forb[0] == i2ID && forb[1] == i1ID) {
-					score -= config.Rows * priority[2]
+					score -= int(w.EnemyPenalty)
 					ignored = append(ignored, i1ID, i2ID)
 				}
 			}
@@ -161,10 +200,7 @@ func tournamentSelection(population [][]int, scores []int64, k int) []int {
 func RunGA(req Request) ([]Response, int64, []int) {
 	N := req.ClassConfig.Columns * req.ClassConfig.Rows
 	popSize, generations := req.PopSize, req.Generations
-	priorities := make([]int, 4)
-	for i := range req.Priority {
-		priorities[req.Priority[i]] = i + 1
-	}
+	weights := calculateWeights(req.ClassConfig, req.PriorityWeights)
 	population := make([][]int, popSize)
 	for i := range population {
 		population[i] = rand.Perm(N)
@@ -173,18 +209,18 @@ func RunGA(req Request) ([]Response, int64, []int) {
 		scores := make([]int64, popSize)
 		ignored := make([][]int, popSize)
 		for i, seat := range population {
-			scores[i], ignored[i] = fitness(seat, req.Students, req.Preferences, req.Forbidden, req.ClassConfig, priorities)
+			scores[i], ignored[i] = fitness(seat, req.Students, req.Preferences, req.Forbidden, req.ClassConfig, weights)
 
 		}
 		newPop := make([][]int, popSize)
-			iBest := 0
-			for j := 1; j < popSize; j++ {
-				if scores[j] > scores[iBest] {
-					iBest = j
-				}
+		iBest := 0
+		for j := 1; j < popSize; j++ {
+			if scores[j] > scores[iBest] {
+				iBest = j
 			}
-			newPop[0] = make([]int, N)
-			copy(newPop[0], population[iBest])
+		}
+		newPop[0] = make([]int, N)
+		copy(newPop[0], population[iBest])
 		for i := 1; i < popSize; i++ {
 			parent1 := tournamentSelection(population, scores, 3)
 			parent2 := tournamentSelection(population, scores, 3)
@@ -198,9 +234,9 @@ func RunGA(req Request) ([]Response, int64, []int) {
 	}
 
 	iBest := 0
-	bestAns, bestIgn := fitness(population[0], req.Students, req.Preferences, req.Forbidden, req.ClassConfig, priorities)
+	bestAns, bestIgn := fitness(population[0], req.Students, req.Preferences, req.Forbidden, req.ClassConfig, weights)
 	for i, seat := range population {
-		Ans, Ign := fitness(seat, req.Students, req.Preferences, req.Forbidden, req.ClassConfig, priorities)
+		Ans, Ign := fitness(seat, req.Students, req.Preferences, req.Forbidden, req.ClassConfig, weights)
 		if Ans > bestAns {
 			bestAns = Ans
 			iBest = i
